794846c6d731f49919bbfcc1784948f16c890a5d
mgag <martin.guthrie.code@gmail.com>
2019-03-09 12:18:51 -0500

    improved tmiserver start script

--- public/tmiserver.sh
+++ public/tmiserver.sh
@@ -1,26 +1,96 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+usage () {
+  echo "Usage: tmiserver.sh [flags] <command>"
+  echo ""
+  echo "command:"
+  echo "  start                     Start TMIServer"
+  echo ""
+  echo "    flags, --restart=, -r   <always|no> (default no) 'always' will start tmiserver EVERY time the"
+  echo "                            computer is booted, which is typically used on a node that"
+  echo "                            is in actual deployment."
+  echo "                            To disable restart, use 'docker update --restart=no tmiserver'"
+  echo "                            and then reboot the node."
+  echo ""
+  echo "  update"
+  echo ""
+}
 
 if [[ $1 == "--help" ]] || [[ $1 == "" ]] ; then
-
-  echo "Usage: tmiserver.sh <restart_always_or_no>"
-  echo ""
-  echo "Notes"
-  echo "1. Restart 'always' will start tmi EVERY time the computer is booted."
-  echo "   To disable restart, use 'docker update --restart=no tmistation'"
-  echo "   and then 'docker ps', 'docker stop <id>', to stop tmistation"
+  usage
   exit 0
-
 fi
 
-RESTART=$1
-HOSTNAME=$(hostname)
+# set defaults here
+flag_restart=no
 
-docker run -d \
-    --net tminet \
-    --hostname=${HOSTNAME} \
-    --restart=${RESTART} \
-    -p 6600:6600 \
-    -v $(pwd):/app/public \
-    --name tmiserver \
-    --rm \
-    mgagcode/tmiserver
+start () {
+    echo start $flag_restart
+    #
+    # docker run --rm and --restart commands are exclusive of each other
+    #
+    if [[ $flag_restart == "always" ]]; then
+        docker run -d \
+            --net tminet \
+            --hostname=${HOSTNAME} \
+            --restart=${flag_restart} \
+            -p 6600:6600 \
+            -v $(pwd):/app/public \
+            --name tmiserver \
+            mgagcode/tmiserver
+    elif [[ $flag_restart == "no" ]]; then
+        docker run -d \
+            --net tminet \
+            --hostname=${HOSTNAME} \
+            -p 6600:6600 \
+            -v $(pwd):/app/public \
+            --name tmiserver \
+            --rm \
+            mgagcode/tmiserver
+    else
+      echo Unexpected restart value, must be always or no, example: --restart=always
+      exit 1
+    fi
+}
+
+docker_pull () {
+    echo docker pull
+    docker pull mgagcode/tmiserver
+    docker pull mgagcode/tmistation
+}
+
+handle_command () {
+  echo $1
+  case $1 in
+    start)
+      start
+      ;;
+
+    update)
+      docker_pull
+      ;;
+
+    *)
+      echo Unknown Command
+      usage
+      exit 1
+      ;;
+
+  esac
+  exit 0
+}
+
+# see https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    -r) flag_restart="$2"; shift 2;;
+
+    --restart=*) flag_restart="${1#*=}"; shift 1;;
+    --restart) echo "$1 requires an argument" >&2; exit 1;;
+#    --restart|--pidfile) echo "$1 requires an argument" >&2; exit 1;;  example of adding more
+
+    -*) echo "unknown option: $1" >&2; exit 1;;
+    *) handle_command "$1"; shift 1;;
+  esac
+done
